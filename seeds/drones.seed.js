5422dc2c26588e8249c07f841d8eb027fce5384b
Milky Raven <heartonllamas@gmail.com>
2022-11-03 12:06:22 +0100

    seeds.js done, ready to run the document

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,41 @@
 // Iteration #1
+const mongoose = require("mongoose")
+const Drone = require("../models/Drone.model")
+
+const MONGO_URI = "mongodb://localhost:27017/day_7_project"
+
+const drones = [
+    {
+        name: "The Zap 3000",
+        propellers: 3,
+        maxSpeed: 20        
+    }, 
+    {
+        name: "TurboQ 2x",
+        propellers: 5,
+        maxSpeed: 30
+    },
+    {
+        name: "Little Lazy 3",
+        propellers: 2,
+        maxSpeed: 10
+    }
+]
+
+const createDrones = async function() {
+    try{
+        const connect = await mongoose.connect(MONGO_URI)
+        console.log(`Connected to database: ${connect.connections[0].name}`)
+        //creating the drones
+        const dbDrones = await Drone.create(drones)
+        console.log(`${dbDrones.length} - drones created `)
+        //closing the connection
+        const dbClose = await mongoose.connection.close()
+        console.log("Connection closed")
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+createDrones();
