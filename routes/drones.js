535a75164276438f61439acca3be0d2a5268102a
Milky Raven <heartonllamas@gmail.com>
2022-11-03 14:38:20 +0100

    editing works

--- routes/drones.js
+++ routes/drones.js
@@ -34,18 +34,41 @@ router.post('/drones/create', async (req, res, next) => {
   catch(err){console.log(err)}
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:droneId/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const droneId = req.params.droneId
+  try {
+    const drone = await Drone.findById(droneId)
+    res.render("drones/update-form", drone)
+  }
+  catch (err) {console.log(err)}
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:droneId/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const drone = req.body;
+  const droneId = req.params.droneId;
+  console.log('this is the IDDDD', droneId)
+  try {
+      const test = await Drone.findByIdAndUpdate(droneId, drone)
+      console.log('this is updated DRONE', test)
+      res.redirect("/drones")
+  }
+  catch(err){
+    console.log(err)}
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/drones/:id/delete', async (req, res, next) => {
   // Iteration #5: Delete the drone
+  const droneId = req.params.droneId
+  try {
+    await Book.findByIdAndDelete(droneId)
+    console.log("Drone deleted!")
+    res.redirect("/drones")
+  }
+  catch(err) {
+    console.log(err)
+  }
   // ... your code here
 });
 
